86e954c150b38b19702eb5d942ad936a3b09d478
Pablo <pablo95lp@gmail.com>
2016-11-28 14:33:47 +0100

    Correigo error en Audio seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(RoleTableSeeder::class);
         $this->call(UserTableSeeder::class);
-        $this->call(AudioTableSeeder::class);
 
         Model::reguard();
     }
